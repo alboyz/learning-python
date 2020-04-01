fd3e92a7916312b90e8a0c56ab1abfad2908a657
ali <alboyzskeaters@gmail.com>
2020-04-01 21:04:14 +0700

    if-else break continoe

--- if-ele.py
+++ if-ele.py
@@ -1,9 +1,22 @@
-grade = float(input("Enter a number: "))
+# # if else
+# grade = float(input("Enter a number: "))
 
-if grade>=90:
-    print("Grade A")
-elif grade>=80:
-    print("Grade B")
-elif grade>=70:
-    print("Grade C")
-else:print("Study better great again")
+# if grade >= 90:
+#     print("Grade A")
+# elif grade >= 80:
+#     print("Grade B")
+# elif grade >= 70:
+#     print("Grade C")
+# else:
+#     print("Study better great again")
+
+# for range
+number = 25
+for i in range(0, 40):
+    print(i)
+    if i is 25:
+        print("angka ditemukan", i)
+        continue
+    if i is 35:
+        print("35 berhenti bro")
+        break
