47c39eeff08eeaea6814c3bd4a4cda2a24da116d
ali <alboyzskeaters@gmail.com>
2020-04-01 10:22:12 +0700

    Data Type in python

---
+++ lesson-python.py
@@ -0,0 +1,9 @@
+# /*Date type*/
+name = "Ali"
+print(name, "is type of", type(name))
+age = 27
+print(age, "is type of", type(age))
+tall = 189.8
+print(tall, "is type of", type(tall))
+pria = True
+print(pria, "istype of", type(pria))
