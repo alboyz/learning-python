bc7a2aeaa28d89075fc41c6f644c5f4cb10bd8db
ali <alboyzskeaters@gmail.com>
2020-04-02 22:14:38 +0700

    class __init__

---
+++ class_phyton/class.py
@@ -0,0 +1,19 @@
+# class
+class Mahasiswa():
+    def __init__(self, input_nama):
+        self.nama = input_nama
+
+    def belajar(self, kondisi):
+        print(self.nama, 'sedang belajar', kondisi)
+
+
+# main programnya
+
+
+name = Mahasiswa("Ali")
+
+
+print(name.nama)
+name.belajar("python3")
+name.nama = "sueb"
+print(name.nama)
